9c904237463d9142a92710e36677bf1c47a7350e
bodeyprovansal <bprovansal@gmail.com>
2020-09-24 16:38:21 -0700

    removed import

--- AutoPip/main.py
+++ AutoPip/main.py
@@ -1,7 +1,5 @@
 import json
 
-from EclipseInterface import EclipseInterface 
-from TestMeasurement import SingleTestMeasurement
 from TestScript import TestScript
 
 newScript = TestScript('script.JSON')
