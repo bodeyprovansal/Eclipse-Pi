2f112d800c1460b0dbd0e0cfc2c0fea041f3c6ff
bodeyprovansal <bprovansal@gmail.com>
2020-09-24 16:53:48 -0700

    TestHandler now runs TestScripts

--- AutoPip/TestHandler.py
+++ AutoPip/TestHandler.py
@@ -1,7 +1,15 @@
 from datetime import datetime
+from TestScript import TestScript
 class TestHandler():
-	def __init()__(TH, testTime, testScripts, testParameters):
+	def __init__(TH):
 		TH.testTime = datetime.now()
-		TH.testTime = testTime
-		TH.testScripts = testScripts
-		TH.testParameters = testParameters
+		TH.testScripts = []
+		
+	def addScript(handler, script):
+		newScript = TestScript(script)
+		handler.testScripts.append(newScript)
+		
+	def runScripts(handler):
+		for script in handler.testScripts:
+			script.runCommands()
+		
--- AutoPip/main.py
+++ AutoPip/main.py
@@ -1,10 +1,17 @@
-import json
+from TestHandler import TestHandler
 
-from TestScript import TestScript
-
-newScript = TestScript('script.JSON')
+#Test Handler starts a new Test
+newTest = TestHandler()
+#Tests are composed of TestScripts
+#TestScripts are composed of TestMeasurements
+#TestMeasurements are composed of:
+	#datetime the measurement was taken
+	#command(s)
+	#a port to receive a command
+	#a friendly name for the device at that port
+	#response(s) from that port
 print("---- Start -----")
-#newScript.showCommands()
-newScript.runCommands()
+newTest.addScript('script.JSON')
+newTest.runScripts()
 print("---- End -----")
 
