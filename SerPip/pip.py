df2a0f798d8653baf277f9c98634e7461cb90383
Bodey Provansal <bprovansal@gmail.com>
2022-08-24 14:32:34 -0700

    Update pip.py

--- SerPip/pip.py
+++ SerPip/pip.py
@@ -11,26 +11,11 @@ URL_POST = "http://18.188.218.25/tests/"
 response_get = requests.get(url=URL_GET)
 #loads in JSON and converts to Python object
 data_in = json.loads(response_get.content)
-#print("GET Response")
-#print(response_get.status_code)
-#print(response_get.json())
-#print(data_in["name"])
-#print(data_in["alias"])
-#print(data_in["id"])
 #takes a Python object and converts to JSON string
 test_obj_out = {
     "name":"API Post Test",
     "alias":"API_POST_01"
 }
-
-#response_post = requests.post(url=URL_POST, json=test_obj_out)
-#data_out = json.loads(response_post.content)
-#print("POST Response")
-#print(response_post.status_code)
-#print(response_post.json())
-#print(data_out["id"])
-#print(data_out["alias"])
-#print(data_out["name"])
 
 def newPostObj(name, alias, command, result):
     post_obj = {
@@ -118,27 +103,4 @@ print(data_out["alias"])
 print(data_out["name"])
 print(data_out["command"])
 print(data_out["result"])
-#setModeResp = writeToPippin("accel set mode enabled", pip)
-#printResponse("Enable Accel Mode", setModeResp)
-#while 1:
-    #print("Writing to port...")
-    #writeToPippin("help accel", pip)
-    
-    #time.sleep(1)
-    #xyzResp = writeToPippin("accel get xyz", pip)
-    #time.sleep(1)
-    #printResponse("Get XYZ: ", xyzResp)
-    #time.sleep(1)
-    #motionResp = writeToPippin("accel get motion", pip)
-    #time.sleep(1)
-    #printResponse("Get Motion: ", motionResp)
-    
-    #upResp = writeToPippin('up', pip)
-    #printResponse("UP\n", upResp)
-    #writeToPippin('help oled', pip)
-    #writeToPippin('help', pip)
-    #time.sleep(3)
-    #userToPippin = raw_input("Enter a command: ")
-    #pipResp = writeToPippin(userToPippin, pip)
-    #printResponse("Pippin Response", pipResp)
     
